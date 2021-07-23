a226896d513bb826bb4a6acc5c0ff25e2f660b8d
Trevor Slobodnick <trevorslobodnick@gmail.com>
2021-07-23 13:45:19 -0400

    Basic set up for the app, removed unnecessary files, replaced the favicon, and created an empty Header component as well as a coming soon component.

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,17 @@
+
+
+const ComingSoon = () => {
+    return (
+        <div>
+            <div class="image-wrapper">
+                <img src="images/pika.png" alt=""/>
+            </div>
+            <h1>Coming Soon</h1>
+            <div class="image-wrapper">
+                <img src="images/pika.png" alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default ComingSoon
